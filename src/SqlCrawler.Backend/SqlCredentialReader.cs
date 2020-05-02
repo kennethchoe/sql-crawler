115348e21da0003ede027daff9e1f5e336f2b77e
Kenneth Choe <kenneth.choe@gmail.com>
2020-05-02 15:47:22 -0500

    spawn out SqlServerInfo

--- src/SqlCrawler.Backend/SqlCredentialReader.cs
+++ src/SqlCrawler.Backend/SqlCredentialReader.cs
@@ -25,27 +25,4 @@ namespace SqlCrawler.Backend
             return records;
         }
     }
-
-    public class SqlServerInfo
-    {
-        public string ServerId { get; set; }
-        public string ServerName { get; set; }
-        public string DataSource { get; set; }
-        public bool UseWindowsAuthentication { get; set; }
-        public string SqlUsername { get; set; }
-        public string SqlPassword { get; set; }
-        public string Description { get; set; }
-
-        public string ToConnectionString()
-        {
-            var result = "Data Source=" + DataSource + ";";
-
-            if (UseWindowsAuthentication)
-                result += "Integrated Security=SSPI;";
-            else
-                result += $"User Id={SqlUsername};Password={SqlPassword};";
-
-            return result;
-        }
-    }
 }
---
+++ src/SqlCrawler.Backend/SqlServerInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace SqlCrawler.Backend
+{
+    public class SqlServerInfo
+    {
+        public string ServerId { get; set; }
+        public string ServerName { get; set; }
+        public string DataSource { get; set; }
+        public bool UseWindowsAuthentication { get; set; }
+        public string SqlUsername { get; set; }
+        public string SqlPassword { get; set; }
+        public string Description { get; set; }
+
+        public string ToConnectionString()
+        {
+            var result = "Data Source=" + DataSource + ";";
+
+            if (UseWindowsAuthentication)
+                result += "Integrated Security=SSPI;";
+            else
+                result += $"User Id={SqlUsername};Password={SqlPassword};";
+
+            return result;
+        }
+    }
+}
