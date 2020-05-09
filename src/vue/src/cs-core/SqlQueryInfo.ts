ec66c4104cb008125f2b6246eea5b5c73993a540
Kenneth Choe <kenneth.choe@gmail.com>
2020-05-09 14:01:18 -0500

    filename case change to support linux

--- src/vue/src/cs-core/SqlQueryInfo.ts
+++
@@ -1,8 +0,0 @@
-﻿
-
-export class sqlQueryInfo {
-    public scope: string | null = null;
-    public name: string | null = null;
-    public query: string | null = null;
-    public lastRetrievedAtUtc: Date | null = null;
-}
