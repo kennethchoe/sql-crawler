37dc46ebaf55b6db081642fa42b951e5686ecd6f
Kenneth Choe <kenneth.choe@gmail.com>
2021-10-14 18:57:25 -0500

    fix for linux

--- run-webapp.sh
+++ run-webapp.sh
@@ -1,3 +1,5 @@
 # execute web app without vue proxying backend
+npm install --prefix src/vue
 npm run build --prefix src/vue
+dotnet dev-certs https
 dotnet run -p ./src/SqlCrawler.Web/SqlCrawler.Web.csproj -v n --launch-profile CypressTest
